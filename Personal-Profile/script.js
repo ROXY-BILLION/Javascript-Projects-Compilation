fdf12033b6e5978e1f6ec66a5388a3c70ef60653
giftdivine <giftdivine787@gmail.com>
2026-08-14 18:38:54 +0100

    initial

---
+++ Personal-Profile/script.js
@@ -0,0 +1,15 @@
+const name = "Okeke Divine Gift";
+const age = 22;
+const profession = "Software Engineer";
+const country = "Nigeria";
+
+const currentYear = 2026;
+const birthYear = currentYear - age;
+
+const introduction = `My name is ${name}. 
+I am ${age} years old.
+I am a ${profession} from ${country}.`;
+
+console.log(introduction);
+
+console.log(`My estimated birth year is ${birthYear}.`);
